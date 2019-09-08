0ed1f45ef25944cfb2c8d9e18c3a67b451a27185
Robin <robinkaris7@gmail.com>
2019-09-08 20:57:20 +0300

    this commit adds a class instance for source

---
+++ app/models.py
@@ -0,0 +1,16 @@
+class Source:
+
+    '''
+    source class to define news object
+    
+    '''
+
+    def __init__(self, id, name,description,url,category):
+
+        self.id = id
+        self.name = name
+        self.description = description
+        self.url = url
+        self.category = category
+    
+    
