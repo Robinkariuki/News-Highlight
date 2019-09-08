be068c42ed4b5a1b7ba8f1a8926cbbb79583b76f
Robin <robinkaris7@gmail.com>
2019-09-08 21:41:11 +0300

    this commit will create template files

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,30 @@
 from flask import Flask
+rom flask import Flask
+from flask_bootstrap import Bootstrap
+from config import config_options
+
+#creating bootstrap instance c
+def create_app(config_name):
+    
+    #create flask app instance c
+    
+    app = Flask(__name__)
+
+    # Creating the app configurations c
+    app.config.from_object(config_options[config_name])
+    # config_options[config_name].init__app(app) c
+
+
+    # initializing flask extention c
+    bootstrap.init_app(app)
+
+    # registering the blueprint c
+    from .main import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    #setting config c
+    from .requests import configure_request
+    configure_request(app)
+
+
+    return app
