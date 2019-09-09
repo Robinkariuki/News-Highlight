03d3fb9f5d1e7146130f38642cc7fcf79ee4ff9a
Robin <robinkaris7@gmail.com>
2019-09-09 13:53:13 +0300

    this commit adds test file

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
-set NEWS_API_KEY='6b59de600f2345189c358dc865a00581'
-set SECRET_KEY= 'Lx{@\x86\x81\x14\x1e\xeb\xda'
+export NEWS_API_KEY='6b59de600f2345189c358dc865a00581'
+export SECRET_KEY= '123456'
 
 python manage.py server
